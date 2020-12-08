c35f4a7cf1e7e9ebf3131778408cadf431af0a7f
zhengdigang <2413466979@qq.com>
2020-12-08 14:09:34 +0800

    Test SourceTree

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello!");
-        System.out.println("I am Jack");
-        System.out.println("I am Jack");
-        System.out.println("I am Jack");
+        System.out.println("Hello World!");
+        System.out.println("I am Rose");
     }
 }
